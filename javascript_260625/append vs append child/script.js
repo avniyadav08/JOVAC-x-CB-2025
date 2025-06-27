6e36c798319b20ddc5841f16e941f1763c1e8ff8
Avni Yadav <145352116+avniyadav08@users.noreply.github.com>
2025-06-27 18:21:34 +0530

    Create script.js

---
+++ javascript_260625/append vs append child/script.js
@@ -0,0 +1,9 @@
+const menuItem = document.getElementById("menuItems");
+menuItem.addEventListener("click", (event) => {
+  if (event.target.tagName === "LI") {
+    console.log("Clicked on:", event.target.textContent);
+  }
+})
+const newItem = document.createElement('li');
+newItem.textContent = "Login To start";
+menuItem.append(newItem);
